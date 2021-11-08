aff4a0eb2343c322275acfbdb7ea896be48c3ea4
sudkul <sudhanshu.kulshrestha@gmail.com>
2021-11-08 14:30:00 +0530

    App running in local Docker

--- Makefile
+++ Makefile
@@ -26,6 +26,6 @@ lint:
 	hadolint Dockerfile
 	# This is a linter for Python source code linter: https://www.pylint.org/
 	# This should be run from inside a virtualenv
-	pylint --disable=R,C,W1203 app.py
+	pylint --disable=R,C,W1309,W1203,W1202,W1201 app.py
 
 all: install lint test
--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+# Assuming the image name is nd9991_p4
+docker build --tag=nd9991_p4_project .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run --name myContainer -it -p 8000:80 nd9991_p4_project 
